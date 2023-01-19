22b25dee689a60b726215b75d371b0d98596180f
GlazyevaValentina <116272374+GlazyevaValentina@users.noreply.github.com>
2023-01-19 15:20:06 +0200

    Lesson15

---
+++ Lesson15/dz11_client.py
@@ -0,0 +1,30 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 12001
+
+def messaging(client: socket.socket):
+    while True:
+        message = input('Enter your message: ')
+        if len(message) > 0:
+            break
+
+    client.send(message.encode('utf-8'))
+    data = client.recv(1024).decode('utf-8')
+    print(f"Server: {str(data)}")
+
+
+try:
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            try:
+                client.connect((HOST, PORT))
+                messaging(client)
+            except ConnectionRefusedError:
+                print('Server not responding.')
+
+
+except KeyboardInterrupt as ex:
+    print('\nServer connection terminated.')
+except Exception as ex:
+    print(ex)
